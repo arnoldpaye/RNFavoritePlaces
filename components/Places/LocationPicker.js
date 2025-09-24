ceddd30046b8489c452c79d88d99ced818487092
Arnold Paye <arnoldpayet@gmail.com>
2025-09-24 15:31:26 -0400

    feat: add address fetching functionality to LocationPicker and enhance location handling

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -13,7 +13,7 @@ import {
 import OutlinedButton from "../UI/OutlinedButton";
 
 import { Colors } from "../../constants/colors";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 
 function LocationPicker({ onPickLocation }) {
   const [pickedLocation, setPickedLocation] = useState();
@@ -39,7 +39,14 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = getAddress(pickedLocation.lat, pickedLocation.lng);
+        onPickLocation({ ...pickedLocation, address });
+      }
+    }
+
+    handleLocation();
   }, [pickedLocation, onPickLocation]);
 
   async function verifyLocationPermissions() {
--- util/location.js
+++ util/location.js
@@ -3,3 +3,15 @@ const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 export function getMapPreview(lat, lng) {
   return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red&7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
 }
+
+export async function getAddress(lat, lng) {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch address");
+  }
+
+  const data = await response.json();
+  return data.result[0].formatted_address;
+}
