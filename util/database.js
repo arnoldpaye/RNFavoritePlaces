517b0e749a63c7be53fc0a095c36ccc559618754
Arnold Paye <arnoldpayet@gmail.com>
2025-09-26 09:58:53 -0400

    feat: implement database integration for place storage and retrieval
    - Updated `Place` model to accept an external `id`.
    - Integrated `insertPlace` for adding places to the database.
    - Added `fetchPlaces` for retrieving places from the database.
    - Refactored `AddPlace` and `AllPlaces` screens to use database operations.

--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
 import * as SQLite from "expo-sqlite";
+import { Place } from "../models/place";
+
+let database;
 
 export async function init() {
-  const database = await SQLite.openDatabaseAsync("places.db");
-  return database.execAsync(
+  database = await SQLite.openDatabaseAsync("places.db");
+
+  await database.execAsync(
     `CREATE TABLE IF NOT EXISTS places(
             id INTEGER PRIMARY KEY NOT NULL,
             title TEXT NOT NULL,
@@ -10,6 +14,48 @@ export async function init() {
             address TEXT NOT NULL,
             lat REAL NOT NULL,
             lng REAL NOT NULL
-    )`,
+    )
+    `,
   );
+}
+
+export async function insertPlace(place) {
+  if (!database) {
+    throw new Error("Database not initialized");
+  }
+
+  return await database.runAsync(
+    `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+    [
+      place.title,
+      place.imageUri,
+      place.address,
+      place.location.lat,
+      place.location.lng,
+    ],
+  );
+}
+
+export async function fetchPlaces() {
+  if (!database) {
+    throw new Error("Database not initialized");
+  }
+
+  const result = await database.getAllAsync("SELECT * FROM places", []);
+  const places = [];
+  for (const dp of result) {
+    places.push(
+      new Place(
+        dp.title,
+        dp.imageUri,
+        {
+          address: dp.address,
+          lat: dp.lat,
+          lng: dp.lng,
+        },
+        dp.id,
+      ),
+    );
+  }
+  return places;
 }
